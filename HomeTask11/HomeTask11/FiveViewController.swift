f32fb312bdf33c2c79a67f14302f8fab5254317b
Darya Kireychik <dsilchencko@gmail.com>
2023-11-08 17:01:23 +0300

    changing View hierarchy

--- HomeTask11/HomeTask11/FiveViewController.swift
+++ HomeTask11/HomeTask11/FiveViewController.swift
@@ -13,7 +13,14 @@ class FiveViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+        
+        var vc = navigationController?.viewControllers
+        vc?.swapAt(3, 2)
+        vc?.swapAt(1, 0)
+        navigationController?.viewControllers = vc!
         
     }
+    
+    
+    
 }
